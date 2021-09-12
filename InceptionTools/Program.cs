1b47439befb5b2791d70c1f8ff685e294fb11f51
Elthial <Elthial@hotmail.com>
2021-09-12 13:55:17 +0100

    Currently attempting to replicate RLE decoder

---
+++ InceptionTools/Program.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace InceptionTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Format 01 Files
+            var BTTLTECH = @"G:\btech\BTTLTECH.ICN";
+            var ANIMATE = @"G:\btech\ANIMATE.ICN";
+            var STARLEAG = @"G:\btech\STARLEAG.ICN";
+            var DESTRUCT = @"G:\btech\DESTRUCT.ICN";
+            var MAP = @"G:\btech\MAP.ICN";
+            var TINYLAND = @"G:\btech\TINYLAND.CMP";
+
+            //Format 02 Files
+            var BTSTATS = @"G:\btech\BTSTATS.CMP";
+            var BTTITLE = @"G:\btech\BTTITLE.CMP";
+            var INFOCOM = @"G:\btech\INFOCOM.CMP";
+            var MECHSHAP = @"G:\btech\MECHSHAP.CMP";
+
+            var FilePath = MAP;
+
+            var TestArray = File.ReadAllBytes(FilePath);
+            var Filename = Path.GetFileNameWithoutExtension(FilePath);
+            var FileSize = BitConverter.ToInt16(TestArray, 0);
+            var Format = TestArray[2];
+
+
+            Console.WriteLine("Header Info");
+            Console.WriteLine("----------------------------");
+            Console.WriteLine($"Filename: {Filename}");
+            Console.WriteLine($"FileSize (minus two bytes): {FileSize}");
+            Console.WriteLine($"RLE Format: Format0{Format}");
+            Console.WriteLine("----------------------------");
+
+            byte[] OutputArray;
+
+            var RLE = new RunLengthEncoding();
+            
+            if(Format.Equals(1))
+            {
+                Console.WriteLine("Using Decompress_Format01");
+                OutputArray = RLE.Decompress_Format01(TestArray, 3);
+            }
+            else
+            {
+                Console.WriteLine("Using Decompress_Format02");
+                OutputArray = RLE.Decompress_Format02(TestArray, 3);
+            }
+             
+
+            var EGA = new EGAEncoder();
+            var imageData = EGA.Write2ModeConverter(TestArray);
+
+
+            int width = 320;
+            int height = 200;
+            using (var bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
+            {
+                Console.WriteLine($"Creating {width} x {height} BMP Image.");
+                int bytecount = 0;
+
+                for (int y = 0; y <= height - 1; y++)
+                {
+                    for (int x = 0; x <= width - 1; x++)
+                    {
+                        bmp.SetPixel(x, y, EGA.GetEGAColour(imageData[bytecount]));
+                        bytecount++;
+                    }
+                }
+                Console.WriteLine("Saving BMP");
+                bmp.Save(@"image.bmp");
+            }
+        }
+    }
+
+    class RunLengthEncoding
+    {
+        public byte[] Decompress_Format01(byte[] CompressedFile, int IndexStart)
+        {
+            int MaxBufferRemaining = 0x7D00;
+            byte[] DecodedBuffer = new byte[MaxBufferRemaining];
+
+            int DecodedBuffer_Index = 0;
+            int CompressedFile_Index = IndexStart;
+
+            Console.WriteLine($"CompressedFile Length: {CompressedFile.Length}");
+
+            GetByteToDecompress:
+
+            uint ByteValue;
+            bool IsZeroByte = false;
+            byte CurrentCompressedByte = (byte)CompressedFile[CompressedFile_Index];
+            
+            if (CurrentCompressedByte != 0x00)
+            {
+                ByteValue = (uint)CurrentCompressedByte;
+                if (CurrentCompressedByte >= 0x00)
+                    goto SetRepeatValue;
+                ByteValue = (uint)-CurrentCompressedByte; //if byte negative flip to positive
+            }
+            else
+            {               
+                CompressedFile_Index++;
+                //ByteValue = BitConverter.ToUInt16(CompressedFile, CompressedFile_Index);      
+                ByteValue = CompressedFile[CompressedFile_Index];
+                CompressedFile_Index++;
+            }
+            //Flag as zero seen
+            IsZeroByte = true;
+
+            SetRepeatValue:
+            //Byte value becomes repeat value
+            uint RepeatCountdown = ByteValue;
+
+            GetNextByte:
+            //Increment to next byte
+            ++CompressedFile_Index;
+            //Next byte will go to buffer
+            byte OutputByte = CompressedFile[CompressedFile_Index];
+
+            do
+            {
+                DecodedBuffer[DecodedBuffer_Index] = OutputByte;
+                ++DecodedBuffer_Index;
+                --MaxBufferRemaining;
+
+                if (MaxBufferRemaining == 0)
+                    return DecodedBuffer;
+
+                if (IsZeroByte)
+                {
+                    --RepeatCountdown;
+                    if (RepeatCountdown != 0)
+                        goto GetNextByte;
+
+                    ++CompressedFile_Index;
+                    goto GetByteToDecompress;
+                }
+                --RepeatCountdown;
+            } while (RepeatCountdown != 0);
+            ++CompressedFile_Index;
+            goto GetByteToDecompress;
+
+        }
+
+        public byte[] Decompress_Format02(byte[] CompresssedFile, int IndexStart)
+        {
+            int MaxBufferRemaining = 0x7D00;
+            byte[] DecodedBuffer = new byte[MaxBufferRemaining];
+
+            int DecodedBuffer_Index = 0;
+            int CompressedFile_Index = IndexStart;
+
+
+            int MaxRepeatLengthRemaining = 200;
+
+            GetByteToDecompress:
+            uint ByteValue;
+            bool IsZeroByte = false;
+            char CurrentCompressedByte = (char)CompresssedFile[CompressedFile_Index];
+
+            if (CurrentCompressedByte != 0x00)
+            {
+                ByteValue = (uint)CurrentCompressedByte;
+                if (CurrentCompressedByte >= 0x00)
+                    goto SetRepeatValue;
+
+                ByteValue = (uint) -CurrentCompressedByte;
+            }
+            else
+            {
+                int CF_newIndex = CompressedFile_Index + 1;
+                ByteValue = CompresssedFile[CF_newIndex];
+                CompressedFile_Index = CF_newIndex + 1;
+            }
+            IsZeroByte = true;
+
+            SetRepeatValue:
+            uint RepeatCountdown = ByteValue;
+
+            GetNextByte:
+            ++CompressedFile_Index;
+            byte OutputByte = CompresssedFile[CompressedFile_Index];
+
+            do
+            {
+                DecodedBuffer[DecodedBuffer_Index] = OutputByte;
+                ++DecodedBuffer_Index;
+                --MaxRepeatLengthRemaining;
+
+                if (MaxRepeatLengthRemaining == 0)
+                {
+                    MaxRepeatLengthRemaining = 200;
+                    DecodedBuffer_Index -= 31999; //How to this map outside of pointers sub di,7CFFh
+                }
+                --MaxBufferRemaining;
+
+                if (MaxBufferRemaining == 0)
+                    return DecodedBuffer;
+
+                if (IsZeroByte)
+                {
+                    --RepeatCountdown;
+                    if (RepeatCountdown != 0x00)
+                        goto GetNextByte;
+
+                    ++CompressedFile_Index;
+                    goto GetByteToDecompress;
+                }
+                --RepeatCountdown;
+
+            } while (RepeatCountdown != 0x00);
+
+            ++CompressedFile_Index;
+            goto GetByteToDecompress;
+        }
+    }
+
+    class EGAEncoder
+    {
+        public byte[] Write2ModeConverter(Byte[] GraphicsArray)
+        {
+            int Width = 320;
+            int Height = 200;
+
+            // (*
+            // 	  Write mode 2
+            // 	  The lower 4 bits of the CPU byte are the bits for each plane. If set for a
+            // 	  plane, all bits selected with the Bitmask register are set, if clear, they're
+            // 	  cleared.
+            // 	  So you can do the same as in the example from mode 0, but with fewer
+            // 	  instructions:
+            // 	  *)	
+
+
+            //This is a screen draw for 320 x 200
+            //320px x 200px / 8bitsperbyte = 8000 Bytes
+
+            //EGA draws 8 pixels at a time, Each pixel is 4 bit (nibble)
+            //Write mode two writes to 8 bits across 4 planes
+            //Write mode two spreads a byte (high & low nibbles) across two bits
+            //Little-endian systems, lower-order bytes precede higher-order bytes
+
+            byte[] VGA_Memory = new byte[Width * Height];
+            int VGA_Offset = 0;
+            int ByteCount = 0;
+
+            foreach (byte b in GraphicsArray)
+            {
+                byte high = (byte)((b >> 4) & 0xF);
+                byte low = (byte)(b & 0x0F);                
+                VGA_Memory[VGA_Offset] = high;
+                ++VGA_Offset;
+                VGA_Memory[VGA_Offset] = low;   
+                ++VGA_Offset;
+                ByteCount++;
+            }
+
+            Console.WriteLine($"Screen Resolution is {Width} x {Height}. Expected Pixels {Width * Height}");
+            Console.WriteLine($"There are {VGA_Memory.Length - VGA_Offset} missing pixels from the VGA register");
+       
+            return VGA_Memory;
+        }
+
+        public Color GetEGAColour(int pixel)
+        {
+            switch(pixel)
+            {
+                case 0: //Black
+                    return Color.FromArgb(0x00, 0x00, 0x00); 
+                case 1: //Blue
+                    return Color.FromArgb(0x00, 0x00, 0xAA);
+                case 2: //Green
+                    return Color.FromArgb(0x00, 0xAA, 0x00);
+                case 3: //Cyan
+                    return Color.FromArgb(0x00, 0xAA, 0xAA);
+                case 4: //Red
+                    return Color.FromArgb(0xAA, 0x00, 0x00);
+                case 5: //Magenta
+                    return Color.FromArgb(0xAA, 0x00, 0xAA);
+                case 6: //Yellow / Brown
+                    return Color.FromArgb(0xAA, 0x55, 0x00);
+                case 7: //White / Light Grey
+                    return Color.FromArgb(0xAA, 0xAA, 0xAA);
+                case 8: //Dark Gray / Bright Black
+                    return Color.FromArgb(0x55, 0x55, 0x55);
+                case 9: //Bright Blue
+                    return Color.FromArgb(0x55, 0x55, 0xFF);
+                case 10: //Bright Green
+                    return Color.FromArgb(0x55, 0xFF, 0x55);
+                case 11: //Bright Cyan
+                    return Color.FromArgb(0x55, 0xFF, 0xFF);
+                case 12: //Bright Red
+                    return Color.FromArgb(0xFF, 0x55, 0x55);
+                case 13: //Bright Magenta
+                    return Color.FromArgb(0xFF, 0x55, 0xFF);
+                case 14: //Bright Yellow
+                    return Color.FromArgb(0xFF, 0xFF, 0x55);
+                case 15: //Bright White
+                    return Color.FromArgb(0xFF, 0xFF, 0xFF);
+                default:
+                    throw new ArgumentOutOfRangeException("Non-EGA colour value detected!");
+            }
+        }
+
+        public byte[] GetTestBitmapData()
+        {
+            //Create the empty image.
+            Bitmap image = new Bitmap(320, 200);
+
+            //draw a useless line for some data
+            Graphics imageData = Graphics.FromImage(image);
+            imageData.DrawLine(new Pen(Color.Red), 0, 0, 320, 200);
+
+            //Convert to byte array
+            MemoryStream memoryStream = new MemoryStream();
+            byte[] bitmapData;
+
+            using (memoryStream)
+            {
+                image.Save(memoryStream, ImageFormat.Bmp);
+                bitmapData = memoryStream.ToArray();
+            }
+            return bitmapData;
+        }
+    }
+}
