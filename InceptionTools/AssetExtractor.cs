36bf7edd4e46868db6e1ffda5d0b5b633b93da0b
Elthial <Elthial@hotmail.com>
2021-09-17 21:33:26 +0100

    Little more refactoring

--- InceptionTools/AssetExtractor.cs
+++ InceptionTools/AssetExtractor.cs
@@ -1,51 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 
 namespace InceptionTools
 {
-
-    class InceptionFile
-    {
-        public InceptionFile(string ImagePath, int ImageWidth)
-        {
-            Name = Path.GetFileNameWithoutExtension(ImagePath);
-            FilePath = ImagePath;
-            Width = ImageWidth;            
-            Contents = File.ReadAllBytes(FilePath);            
-            Size = BitConverter.ToInt16(Contents, 0);
-            Format = Contents[2];
-            StartPos = 3;
-        }
-        public string Name { get; }
-        public string FilePath { get; }
-        public int Width { get; }
-        public int Format { get; }
-        public int Size { get; }
-        public int StartPos { get; }
-        public byte[] Contents { get; }
-
-        //Palette
-    }
 
     public class AssetExtractor
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public void ExtractToFileSystem()
         {
-            var CHI_Files = new List<InceptionFile>()
+            var CHI_Files = new List<InceptionImageFile>()
             {
-                new InceptionFile(@"G:\btech\BTTLTECH.ICN", 16),
-                new InceptionFile(@"G:\btech\BTBORDER.CMP", 320),
-                new InceptionFile(@"G:\btech\ANIMATE.ICN", 16),
-                new InceptionFile(@"G:\btech\STARLEAG.ICN", 16),
-                new InceptionFile(@"G:\btech\DESTRUCT.ICN", 16),
-                new InceptionFile(@"G:\btech\MAP.ICN", 16),
-                new InceptionFile(@"G:\btech\TINYLAND.CMP", 320),
-                new InceptionFile(@"G:\btech\BTSTATS.CMP", 320),
-                new InceptionFile(@"G:\btech\BTTITLE.CMP", 320),
-                new InceptionFile(@"G:\btech\INFOCOM.CMP", 320),
-                new InceptionFile(@"G:\btech\MECHSHAP.CMP", 320)
+                new InceptionImageFile(@"G:\btech\BTTLTECH.ICN"),
+                new InceptionImageFile(@"G:\btech\BTBORDER.CMP"),
+                new InceptionImageFile(@"G:\btech\ANIMATE.ICN"),
+                new InceptionImageFile(@"G:\btech\STARLEAG.ICN"),
+                new InceptionImageFile(@"G:\btech\DESTRUCT.ICN"),
+                new InceptionImageFile(@"G:\btech\MAP.ICN"),
+                new InceptionImageFile(@"G:\btech\TINYLAND.CMP"),
+                new InceptionImageFile(@"G:\btech\BTSTATS.CMP"),
+                new InceptionImageFile(@"G:\btech\BTTITLE.CMP"),
+                new InceptionImageFile(@"G:\btech\INFOCOM.CMP"),
+                new InceptionImageFile(@"G:\btech\MECHSHAP.CMP")
             };
 
             foreach(var f in CHI_Files)
---
+++ InceptionTools/FileTypes/InceptionImageFile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace InceptionTools
+{
+    class InceptionImageFile
+    {
+        public InceptionImageFile(string FilePath)
+        {            
+            Path = FilePath;
+            Name = System.IO.Path.GetFileNameWithoutExtension(FilePath);
+            Extension = System.IO.Path.GetExtension(FilePath);
+            Width = Extension.Equals("CMP") ? 320 : 16;
+            Contents = File.ReadAllBytes(Path);            
+            Size = BitConverter.ToInt16(Contents, 0);
+            Format = Contents[2];
+            StartPos = 3;
+
+            if (!(Extension.Equals("CMP") || Extension.Equals("ICN")))
+            {
+                throw new ArgumentException("Image file extensions must be CMP or ICN");
+            }
+        }
+        public string Name { get; }
+        public string Path { get; }
+        public string Extension { get; }
+        public int Width { get; }
+        public int Format { get; }
+        public int Size { get; }
+        public int StartPos { get; }
+        public byte[] Contents { get; }
+
+        //Support Palette swapping
+        //Change one colour for another
+        //Infocom needs 5 (AA00AA) swapped for 41 (5500FF)
+        //BTTitle needs 1 (0000AA) swapped for 0 (000000)
+    }
+}
